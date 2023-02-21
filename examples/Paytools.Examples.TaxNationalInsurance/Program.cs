ec210b8e9659c12823471e6393407116a2ec8e74
paytools-uk <116939215+paytools-uk@users.noreply.github.com>
2023-02-21 17:27:24 +0100

    Added example "licence"

--- examples/Paytools.Examples.TaxNationalInsurance/Program.cs
+++ examples/Paytools.Examples.TaxNationalInsurance/Program.cs
@@ -1,4 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿//
+// ##################### PAYTOOLS EXAMPLE SOURCE CODE ##########################
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this example code (the "Example"), to deal in the Example without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Example, and to permit persons
+// to whom the Example is furnished to do so without constraint.
+
+// THE EXAMPLE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE EXAMPLE OR THE USE OR OTHER DEALINGS IN THE EXAMPLE.
+
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Paytools.ReferenceData;
 using System.Diagnostics;
