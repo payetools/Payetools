b200c33bb84ab522519bbca5074b39ffabfbcf18
paytools-reviewer <128211401+paytools-reviewer@users.noreply.github.com>
2023-03-30 08:18:42 +0100

    fix: updating path

--- .github/runtests.sh
+++ .github/runtests.sh
@@ -1,2 +1,2 @@
 TESTPROJECTS=$(find test -name "Paytools.*.Tests")
-for PROJECT in $TESTPROJECTS; do "dotnet test $PROJECT --no-build"; done
+for PROJECT in $TESTPROJECTS; do "dotnet test ./$PROJECT --no-build"; done
