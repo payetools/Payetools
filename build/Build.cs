eb3de698bfd063add699b8f9e2192265d2023d63
payetools <143715134+payetools@users.noreply.github.com>
2024-01-02 21:33:57 +0100

    ci: added Nuke support

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,20 @@
-using System;
-using System.Linq;
+// Copyright (c) 2023 Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under one of the following licenses:
+//
+//   * GNU Affero General Public License, see https://www.gnu.org/licenses/agpl-3.0.html
+//   * Payetools Commercial Use license [TBA]
+//
+// For further information on licensing options, see https://paytools.dev/licensing-paytools.html
+
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MinVer;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
@@ -19,26 +24,74 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
+
+    [GitRepository]
+    readonly GitRepository GitRepository;
+
+    [MinVer]
+    readonly MinVer MinVer;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+
+    AbsolutePath TestDirectory => RootDirectory / "test";
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(d => d.DeleteDirectory());
+            TestDirectory.GlobDirectories("**/bin", "**/obj").ForEach(d => d.DeleteDirectory());
+            ArtifactsDirectory.CreateOrCleanDirectory();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
         });
 
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetFilter("FullyQualifiedName!~Payetools.Testing")
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+                .SetIncludeSymbols(true)
+                .SetOutputDirectory(ArtifactsDirectory)
+                .EnableNoRestore()
+                .EnableNoBuild());
+        });
 }
