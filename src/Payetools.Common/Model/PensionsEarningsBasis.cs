205327233d92cae6308e73826ec2e8841a8f51c0
payetools <143715134+payetools@users.noreply.github.com>
2024-02-05 11:06:17 +0100

    feat: PensionsEarningsBasis marked with [Flags} attribute
    BREAKING CHANGE: PensionsEarningsBasis enum is marked with the [Flags] attribute so the enum values have changed

--- src/Payetools.Common/Model/PensionsEarningsBasis.cs
+++ src/Payetools.Common/Model/PensionsEarningsBasis.cs
@@ -9,10 +9,13 @@ namespace Payetools.Common.Model;
 /// <summary>
 /// Represents the earnings basis which is used to calculate pension contributions.
 /// </summary>
+/// <remarks>Note that this enum is marked with the [Flags] attribute so that it can be used as an indicator for
+/// pension schemes to indicate which earnings bases they support.</remarks>
+[Flags]
 public enum PensionsEarningsBasis
 {
     /// <summary>Not specified.</summary>
-    Unspecified,
+    Unspecified = 0,
 
     /// <summary>Qualifying Earnings includes salary, wages, commission, bonuses, overtime, statutory sick pay, statutory maternity pay,
     /// ordinary or additional statutory paternity pay and statutory adoption pay.
@@ -20,18 +23,28 @@ public enum PensionsEarningsBasis
     /// Qualifying earnings is calculated from pensionable pay deducting a lower threshold and capping at an upper threshold, the
     /// thresholds being decided by Government each year.
     /// </summary>
-    QualifyingEarnings,
+    QualifyingEarnings = 1,
 
     /// <summary>Pensionable Pay Set 1 contributions are worked out on at least basic pay. Basic pay includes at a minimum, earnings
     /// before deductions, holiday pay and statutory benefits such as maternity, paternity, adoption and sick pay delivered through
     /// payroll.</summary>
-    PensionablePaySet1,
+    PensionablePaySet1 = 2,
 
     /// <summary>Pensionable Pay Set 2 contributions are worked out on at least basic pay, but the key difference between this and
     /// Set 1 is that basic pay must make up at least 85% of total earnings. The employer must monitor this.</summary>
-    PensionablePaySet2,
+    PensionablePaySet2 = 4,
 
     /// <summary>Pensionable Pay Set 2 contributions includes salary, wages, commission, bonuses, overtime, statutory sick pay,
     /// statutory maternity pay, ordinary or additional statutory paternity pay and statutory adoption pay.</summary>
-    PensionablePaySet3
+    PensionablePaySet3 = 8,
+
+    /// <summary>
+    /// Only PensionablePaySets 1 through 3 are supported.
+    /// </summary>
+    PensionablePaySetsOnly = 14,
+
+    /// <summary>
+    /// All earnings bases are supported..
+    /// </summary>
+    All = 15
 }
