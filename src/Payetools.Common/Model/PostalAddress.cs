838ba3f01250e7aa4851f27f576124b509abacf8
payetools <143715134+payetools@users.noreply.github.com>
2024-03-18 09:43:58 +0100

    tidy: made PostalAddress immutable
    BREAKING CHANGE: PostalAddress must be initialised via constructor or initialisers; it is now an immutable type.

--- src/Payetools.Common/Model/PostalAddress.cs
+++ src/Payetools.Common/Model/PostalAddress.cs
@@ -13,21 +13,48 @@ namespace Payetools.Common.Model;
 /// </summary>
 public class PostalAddress
 {
-    /// <summary>Gets or sets the first line of the address.</summary>
-    public string AddressLine1 { get; set; } = null!;
+    /// <summary>Gets the first line of the address.</summary>
+    public string AddressLine1 { get; init; }
 
-    /// <summary>Gets or sets the second line of the address.</summary>
-    public string AddressLine2 { get; set; } = null!;
+    /// <summary>Gets the second line of the address.</summary>
+    public string AddressLine2 { get; init; }
 
-    /// <summary>Gets or sets the third line of the address.</summary>
-    public string? AddressLine3 { get; set; }
+    /// <summary>Gets the third line of the address.</summary>
+    public string? AddressLine3 { get; init; }
 
-    /// <summary>Gets or sets the fourth line of the address.</summary>
-    public string? AddressLine4 { get; set; }
+    /// <summary>Gets the fourth line of the address.</summary>
+    public string? AddressLine4 { get; init; }
 
-    /// <summary>Gets or sets the postcode (UK addresses only).</summary>
-    public UkPostcode? Postcode { get; set; }
+    /// <summary>Gets the postcode (UK addresses only).</summary>
+    public UkPostcode? Postcode { get; init; }
 
-    /// <summary>Gets or sets the foreign country (non-UK addresses only).</summary>
-    public string? ForeignCountry { get; set; }
+    /// <summary>Gets the foreign country (non-UK addresses only).</summary>
+    public string? ForeignCountry { get; init; }
+
+    /// <summary>
+    /// Initialises a new instance of <see cref="PostalAddress"/>.
+    /// </summary>
+    /// <param name="addressLine1">First line of the address. Mandatory.</param>
+    /// <param name="addressLine2">Second line of the address. Mandatory.</param>
+    /// <param name="addressLine3">Third line of the address. Optional, i.e., may be null.</param>
+    /// <param name="addressLine4">Fourth line of the address. Optional, i.e., may be null.</param>
+    /// <param name="postcode">Postcode for UK addresses only.  Should be set to null for overseas
+    /// addresses.</param>
+    /// <param name="foreignCountry">Foreign country for overseas addresses.  Should be null for UK
+    /// addresses.</param>
+    public PostalAddress(
+        string addressLine1,
+        string addressLine2,
+        string? addressLine3,
+        string? addressLine4,
+        UkPostcode? postcode,
+        string? foreignCountry)
+    {
+        AddressLine1 = addressLine1;
+        AddressLine2 = addressLine2;
+        AddressLine3 = addressLine3;
+        AddressLine4 = addressLine4;
+        Postcode = postcode;
+        ForeignCountry = foreignCountry;
+    }
 }
