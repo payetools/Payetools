ae4d1fe81a0a082545a5f7ee3155ce6e8b7f7df8
payetools <143715134+payetools@users.noreply.github.com>
2024-03-19 13:35:52 +0100

    feat: added an enum for the normal hours worked for an employee

---
+++ src/Payetools.Common/Model/NormalHoursWorkedBand.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Common.Model;
+
+/// <summary>
+/// Enumeration that is used to indicate a band of normal hours worked by an employee.
+/// </summary>
+public enum NormalHoursWorkedBand
+{
+    /// <summary>Up to 15.99 hours</summary>
+    A,
+
+    /// <summary>16 to 23.99 hours</summary>
+    B,
+
+    /// <summary>24 to 29.99 hours</summary>
+    C,
+
+    /// <summary>30 hours or more</summary>
+    D,
+
+    /// <summary>Other</summary>
+    E
+}
