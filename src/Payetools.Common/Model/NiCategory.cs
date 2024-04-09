938ae21720bb61739554940720dd5ed3ea618152
payetools <143715134+payetools@users.noreply.github.com>
2024-04-09 14:18:57 +0200

    feat: added new letters for 2024-2025 (D, E, K, N) plus parse helper method

--- src/Payetools.Common/Model/NiCategory.cs
+++ src/Payetools.Common/Model/NiCategory.cs
@@ -25,6 +25,12 @@ public enum NiCategory
     /// <summary>Employees over the State Pension age.</summary>
     C,
 
+    /// <summary>Investment Zone — deferment.  Added for 2024-2025.</summary>
+    D,
+
+    /// <summary>Investment Zone — married women and widows reduced rate.  Added for 2024-2025.</summary>
+    E,
+
     /// <summary>All employees who work in freeports, apart from those in categories I, L, and S.</summary>
     F,
 
@@ -37,11 +43,17 @@ public enum NiCategory
     /// <summary>Employees who can defer National Insurance because they’re already paying it in another job.</summary>
     J,
 
+    /// <summary>Investment Zone — state pensioner.  Added for 2024-2025.</summary>
+    K,
+
     /// <summary>Employees who work in freeports and can defer National Insurance because they’re already paying it in another job.</summary>
     L,
 
     /// <summary>Employees under 21.</summary>
     M,
+
+    /// <summary>Investment Zone.  Added for 2024-2025.</summary>
+    N,
 
     /// <summary>Employees who work in freeports and are over the State Pension age.</summary>
     S,
@@ -54,4 +66,39 @@ public enum NiCategory
 
     /// <summary>Employees under 21 who can defer National Insurance because they’re already paying it in another job.</summary>
     Z
+}
+
+/// <summary>
+/// Extension methods related to <see cref="NiCategory"/>.
+/// </summary>
+public static class NiCategoryRelatedExtensions
+{
+    /// <summary>
+    /// Converts the supplied string into an <see cref="NiCategory"/> value based on the
+    /// specified tax year.  (Not all NI letters are valid for all tax years.)
+    /// </summary>
+    /// <param name="value">String value to convert.  May be null.</param>
+    /// <param name="taxYearEnding">Tax year ending that pertains to this conversion.</param>
+    /// <returns>NI category value that corresponds to the supplied string.</returns>
+    /// <exception cref="ArgumentException">Thrown if the supplied value cannot be converted
+    /// to a valid NI category for the specified tax year.</exception>
+    public static NiCategory? ToNiCategory(this string? value, TaxYearEnding taxYearEnding)
+    {
+        if (value == null)
+            return null;
+
+        if (!Enum.TryParse<NiCategory>(value, out var category))
+            throw new ArgumentException($"Value '{value}' is not a valid NICategory value", nameof(value));
+
+        if (taxYearEnding < TaxYearEnding.Apr5_2025)
+        {
+            if (category == NiCategory.D ||
+                category == NiCategory.E ||
+                category == NiCategory.K ||
+                category == NiCategory.N)
+                throw new ArgumentException("NI Categories D, E, K and N are not valid NI categories prior to tax year ending 5 April 2025", nameof(value));
+        }
+
+        return category;
+    }
 }
