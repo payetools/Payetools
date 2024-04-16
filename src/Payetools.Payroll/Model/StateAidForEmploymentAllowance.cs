b988e2305450d337a62394376c0acf9ee079631d
payetools <143715134+payetools@users.noreply.github.com>
2024-04-16 16:20:41 +0200

    feat: added enum for employment allowance de minimis state aid

---
+++ src/Payetools.Payroll/Model/StateAidForEmploymentAllowance.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Payroll.Model;
+
+/// <summary>
+/// Enumeration used to indicate whether De Minimis State Aid rules apply with regard to Employment Allowance.
+/// </summary>
+public enum StateAidForEmploymentAllowance
+{
+    /// <summary>This value should be selected if the employer is involved in economic activity and the
+    /// business is in the agriculture sector.</summary>
+    Agriculture,
+
+    /// <summary>This value should be selected if the employer is involved in economic activity and the
+    /// business is in the fisheries and aquaculture sector.</summary>
+    FisheriesAndAquaculture,
+
+    /// <summary>This value should be selected if the employer is involved in economic activity and the
+    /// business is in the road transport sector.</summary>
+    RoadTransport,
+
+    /// <summary>This value should be selected if the employer is undertaking economic activity and none
+    /// Agriculture, FisheriesAndAquaculture or RoadTransport apply.</summary>
+    IndustrialOther,
+
+    /// <summary>This value should be selected if the employer is not undertaking economic activity, for
+    /// example, charities, community amateur sports clubs, employing someone to provide personal care.
+    /// Here, de minimis state aid rules do not apply.</summary>
+    NotApplicable
+}
