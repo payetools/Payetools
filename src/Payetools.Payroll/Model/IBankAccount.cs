12e88acb2aeca42ea2c6ea3e2224064c98b99f2f
payetools <143715134+payetools@users.noreply.github.com>
2024-04-16 16:36:51 +0200

    feat: added bank account type (for UK accounts)

---
+++ src/Payetools.Payroll/Model/IBankAccount.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Payroll.Model;
+
+/// <summary>
+/// Interface that represents a UK bank account.
+/// </summary>
+public interface IBankAccount
+{
+    /// <summary>
+    /// Gets the account holder name.
+    /// </summary>
+    string AccountName { get; }
+
+    /// <summary>
+    /// Gets the account number. Should be 8 characters, all numeric, with leading zeroes where appropriate. 
+    /// </summary>
+    string AccountNumber { get; }
+
+    /// <summary>
+    /// Gets the sort code for the account.  Should be 6 characters, all numeric, with leading zeroes where
+    /// appropriate.
+    /// </summary>
+    string SortCode { get; }
+
+    /// <summary>
+    /// Gets the optional building society reference, where appropriate.
+    /// </summary>
+    string? BuildingSocietyReference { get; }
+}
