e6696991315a39ac462b2c695d1cfe8f8f2a927a
payetools <143715134+payetools@users.noreply.github.com>
2024-04-17 17:45:42 +0200

    feat: initial work on statutory payments (wip)

---
+++ src/Payetools.Statutory/Common/IStatutoryPayment.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2023-2024, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Statutory.Common;
+
+/// <summary>
+/// Interface that represents a statutory payment (e.g. SSP. SMP, etc).
+/// </summary>
+internal interface IStatutoryPayment
+{
+}
