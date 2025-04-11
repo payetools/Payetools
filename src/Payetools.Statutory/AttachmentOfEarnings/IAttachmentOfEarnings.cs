c7cf088f279bea04e5d4a92f65f39889c7bfdf12
payetools <143715134+payetools@users.noreply.github.com>
2025-04-11 09:13:51 +0200

    feat: added attachment of earnings interface (wip)

---
+++ src/Payetools.Statutory/AttachmentOfEarnings/IAttachmentOfEarnings.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2023-2025, Payetools Foundation.
+//
+// Payetools Foundation licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace Payetools.Statutory.AttachmentOfEarnings;
+
+/// <summary>
+/// Interface that represents an attachment of earnings order.
+/// </summary>
+public interface IAttachmentOfEarnings
+{
+    /// <summary>
+    /// Gets the type of attachment of earnings order.
+    /// </summary>
+    AttachmentOfEarningsType AttachmentOfEarningsType { get; }
+
+    /// <summary>
+    /// Gets the date from which this attachment of earnings order is effective.
+    /// </summary>
+    DateTime EffectiveDate { get; }
+}
